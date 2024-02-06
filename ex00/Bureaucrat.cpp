5b1c3ca1ad38f167a7f0a79a1d5cdd51aa209496
Pastilhex <ivodanielmarques@gmail.com>
2024-02-06 10:34:42 +0000

    working

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ialves-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 06:01:20 by ialves-m          #+#    #+#             */
-/*   Updated: 2024/02/05 07:13:28 by ialves-m         ###   ########.fr       */
+/*   Updated: 2024/02/05 16:12:31 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Bureaucrat::Bureaucrat(std::string name, int grade) : name(name), grade(grade){
         throw "Bureaucrat::GradeTooHighException";
     else if (grade > 150)
         throw "Bureaucrat::GradeTooLowException";
-    else if ()
+
 }
 
 Bureaucrat::~Bureaucrat(){
@@ -30,4 +30,3 @@ std::string Bureaucrat::getName(){
 int Bureaucrat::getGrade(){
     return this->grade;
 }
-
